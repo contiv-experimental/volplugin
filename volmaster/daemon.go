66b56300b3ede18faf7f12601ae47c20356e912b
Erik Hollensbe <github@hollensbe.org>
2015-07-20 07:33:49 +0000

    volmaster: fleshing out the daemon -> request/response cycle
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- volmaster/daemon.go
+++ volmaster/daemon.go
@@ -1,5 +1,62 @@
 package main
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
+)
+
+type request struct {
+	tenant string
+}
+
 func daemon(config Config) {
+	r := mux.NewRouter()
+	r.HandleFunc("/", config.handleRequest).Methods("POST")
+
+	http.ListenAndServe(":8000", r)
+
 	select {}
+}
+
+func (config Config) handleRequest(w http.ResponseWriter, r *http.Request) {
+	content, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		httpError(w, "Reading request", err)
+		return
+	}
+
+	var req request
+
+	if err := json.Unmarshal(content, &req); err != nil {
+		httpError(w, "Unmarshalling request", err)
+		return
+	}
+
+	if req.tenant == "" {
+		httpError(w, "Reading tenant", errors.New("tenant was blank"))
+		return
+	}
+
+	log.Infof("Request for tenant %q", req.tenant)
+
+	tenConfig, ok := config[req.tenant]
+	if !ok {
+		log.Infof("Request for tenant %q cannot be satisfied: not found", req.tenant)
+		httpError(w, "Handling request", fmt.Errorf("Tenant %q not found", req.tenant))
+		return
+	}
+
+	content, err = json.Marshal(tenConfig)
+	if err != nil {
+		httpError(w, "Marshalling response", err)
+		return
+	}
+
+	w.Write(content)
 }
--- volmaster/utils.go
+++ volmaster/utils.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
 )
 
@@ -11,4 +13,11 @@ func errExit(ctx *cli.Context, err error) {
 	fmt.Printf("\nError: %v\n\n", err)
 	cli.ShowAppHelp(ctx)
 	os.Exit(1)
+}
+
+func httpError(w http.ResponseWriter, message string, err error) {
+	fullError := fmt.Sprintf("%s %v", message, err)
+
+	log.Warnf("Returning HTTP error handling plugin negotiation: %s", fullError)
+	http.Error(w, fullError, http.StatusInternalServerError)
 }
