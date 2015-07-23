1b86688094a672dd15d11c5ec312c366954f022c
Erik Hollensbe <github@hollensbe.org>
2015-07-23 21:39:10 +0000

    volmaster: snapshot intent moved to own file, for later re-digestion
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- volmaster/daemon.go
+++ volmaster/daemon.go
@@ -7,11 +7,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"sync"
-	"time"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/contiv/volplugin/cephdriver"
-	"github.com/contiv/volplugin/librbd"
 	"github.com/gorilla/mux"
 )
 
@@ -24,45 +21,6 @@ var (
 	mutex     = new(sync.Mutex)
 	volumeMap = map[string]map[string]struct{}{} // tenant to array of volume names
 )
-
-func handleSnapshots(config config) {
-	for {
-		log.Debug("Running snapshot supervisor")
-
-		for tenant, value := range config {
-			mutex.Lock()
-			duration, err := time.ParseDuration(config[tenant].Snapshot.Frequency)
-			if err != nil {
-				panic(fmt.Sprintf("Runtime configuration incorrect; cannot use %q as a snapshot frequency", config[tenant].Snapshot.Frequency))
-			}
-
-			if value.UseSnapshots && time.Now().Unix()%int64(duration.Seconds()) == 0 {
-				for _, volumes := range volumeMap {
-					rbdConfig, err := librbd.ReadConfig("/etc/rbdconfig.json")
-					if err != nil {
-						log.Errorf("Cannot read RBD configuration: %v", err)
-						break
-					}
-					driver, err := cephdriver.NewCephDriver(rbdConfig, config[tenant].Pool)
-					if err != nil {
-						log.Errorf("Cannot snap volumes for tenant %q: %v", tenant, err)
-						break
-					}
-					for volume := range volumes {
-						now := time.Now()
-						log.Infof("Snapping volume \"%s/%s\" at %v", tenant, volume, now)
-						if err := driver.NewVolume(volume, config[tenant].Size).CreateSnapshot(now.String()); err != nil {
-							log.Errorf("Cannot snap volume %q: %v", volume, err)
-						}
-					}
-				}
-			}
-			mutex.Unlock()
-		}
-
-		time.Sleep(1 * time.Second)
-	}
-}
 
 func daemon(config config) {
 	r := mux.NewRouter()
---
+++ volmaster/snapshot_intent.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/contiv/volplugin/cephdriver"
+	"github.com/contiv/volplugin/librbd"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+func handleSnapshots(config config) {
+	for {
+		log.Debug("Running snapshot supervisor")
+
+		for tenant, value := range config {
+			mutex.Lock()
+			duration, err := time.ParseDuration(config[tenant].Snapshot.Frequency)
+			if err != nil {
+				panic(fmt.Sprintf("Runtime configuration incorrect; cannot use %q as a snapshot frequency", config[tenant].Snapshot.Frequency))
+			}
+
+			if value.UseSnapshots && time.Now().Unix()%int64(duration.Seconds()) == 0 {
+				for _, volumes := range volumeMap {
+					rbdConfig, err := librbd.ReadConfig("/etc/rbdconfig.json")
+					if err != nil {
+						log.Errorf("Cannot read RBD configuration: %v", err)
+						break
+					}
+					driver, err := cephdriver.NewCephDriver(rbdConfig, config[tenant].Pool)
+					if err != nil {
+						log.Errorf("Cannot snap volumes for tenant %q: %v", tenant, err)
+						break
+					}
+					for volume := range volumes {
+						now := time.Now()
+						log.Infof("Snapping volume \"%s/%s\" at %v", tenant, volume, now)
+						if err := driver.NewVolume(volume, config[tenant].Size).CreateSnapshot(now.String()); err != nil {
+							log.Errorf("Cannot snap volume %q: %v", volume, err)
+						}
+					}
+				}
+			}
+			mutex.Unlock()
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
