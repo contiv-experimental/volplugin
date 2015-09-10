b7d3526184e65d41f43485d24244e47d46531739
Erik Hollensbe <erik+github@hollensbe.org>
2015-09-10 03:40:53 -0700

    systemtests: add a test for volmaster operations on several hosts
    Signed-off-by: Erik Hollensbe <erik+github@hollensbe.org>

--- systemtests/basic_test.go
+++ systemtests/basic_test.go
@@ -39,3 +39,32 @@ func TestVolumeCreate(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestVolumeCreateMultiHost(t *testing.T) {
+	hosts := []string{"mon0", "mon1", "mon2"}
+	defer func() {
+		for _, host := range hosts {
+			t.Logf("Purging %s", host)
+
+			if err := nodeMap[host].RunCommand("docker volume rm " + host); err != nil {
+				t.Fatal(err)
+			}
+
+			if err := nodeMap[host].RunCommand("sudo rbd snap purge " + host + " && sudo rbd rm " + host); err != nil {
+				t.Fatal(err)
+			}
+		}
+	}()
+
+	for _, host := range []string{"mon0", "mon1", "mon2"} {
+		t.Logf("Creating %s", host)
+
+		if err := nodeMap[host].RunCommand("docker volume create -d tenant1 --name " + host); err != nil {
+			t.Fatal(err)
+		}
+
+		if err := nodeMap[host].RunCommand("sudo rbd list | grep -q " + host); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
