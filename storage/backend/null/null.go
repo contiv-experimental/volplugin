1b57b27160400e87c5c27c5eb21fa105412f4d7c
Madhav Puri <madhav.puri@gmail.com>
2016-04-11 07:09:20 -0700

    add state to null driver for enabling tests
    Signed-off-by: Madhav Puri <madhav.puri@gmail.com>

--- storage/backend/null/null.go
+++ storage/backend/null/null.go
@@ -1,104 +1,162 @@
 package null
 
 import (
+	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
 	"time"
 
+	log "github.com/Sirupsen/logrus"
+	"github.com/contiv/errored"
 	"github.com/contiv/volplugin/storage"
 )
 
 // BackendName is string for no-op storage backend
 const BackendName = "null"
 
+// gNullDriver is the singleton driver instace to keep all state in memory
+var gNullDriver *Driver
+
+func getFunctionName() string {
+	if pc, _, _, ok := runtime.Caller(1); ok {
+		f := strings.Split(runtime.FuncForPC(pc).Name(), "/")
+		return f[len(f)-1]
+	}
+	return "unknown"
+}
+
 // Driver implements a no-op storage driver for volplugin.
 //
 // This is intended for regressing volplugin
 type Driver struct {
 	mountpath string
+	created   map[string]bool
+	mounted   map[string]bool
 }
 
 // NewDriver is a generator for Driver structs. It is used by the storage
 // framework to yield new drivers on every creation.
 func NewDriver(mountpath string) storage.Driver {
-	return &Driver{mountpath: mountpath}
+	log.Infof("In %s", getFunctionName())
+	if gNullDriver != nil {
+		return gNullDriver
+	}
+	gNullDriver = &Driver{
+		mountpath: mountpath,
+		created:   make(map[string]bool),
+		mounted:   make(map[string]bool),
+	}
+	return gNullDriver
 }
 
 // Name returns the null backend string
 func (d *Driver) Name() string {
+	log.Infof("In %q", getFunctionName())
 	return BackendName
 }
 
-// Create is a noop.
-func (d *Driver) Create(storage.DriverOptions) error {
+// Create records created volume.
+func (d *Driver) Create(do storage.DriverOptions) error {
+	log.Infof("In %q", getFunctionName())
+	d.created[filepath.Join(d.mountpath, do.Volume.Params["pool"], do.Volume.Name)] = true
 	return nil
 }
 
 // Format is a noop.
 func (d *Driver) Format(do storage.DriverOptions) error {
+	log.Infof("In %q", getFunctionName())
 	return nil
 }
 
-// Destroy is a noop.
+// Destroy deletes volume entry
 func (d *Driver) Destroy(do storage.DriverOptions) error {
+	log.Infof("In %q", getFunctionName())
+	delete(d.created, filepath.Join(d.mountpath, do.Volume.Params["pool"], do.Volume.Name))
 	return nil
 }
 
 // List Volumes returns empty list.
 func (d *Driver) List(lo storage.ListOptions) ([]storage.Volume, error) {
+	log.Infof("In %q", getFunctionName())
 	return []storage.Volume{}, nil
 }
 
-// Mount is a noop
+// Mount records mounted volume
 func (d *Driver) Mount(do storage.DriverOptions) (*storage.Mount, error) {
+	log.Infof("In %q", getFunctionName())
+	if err := os.MkdirAll(d.mountpath, 0700); err != nil && !os.IsExist(err) {
+		return nil, errored.Errorf("error creating %q directory: %v", d.mountpath, err)
+	}
+
+	volumePath := filepath.Join(d.mountpath, do.Volume.Params["pool"], do.Volume.Name)
+	if err := os.MkdirAll(volumePath, 0700); err != nil && !os.IsExist(err) {
+		return nil, errored.Errorf("error creating %q directory: %v", volumePath, err)
+	}
+	d.mounted[filepath.Join(d.mountpath, do.Volume.Params["pool"], do.Volume.Name)] = true
 	return nil, nil
 }
 
-// Unmount is a noop.
+// Unmount deleted mounted volume entry.
 func (d *Driver) Unmount(do storage.DriverOptions) error {
+	log.Infof("In %q", getFunctionName())
+	delete(d.mounted, filepath.Join(d.mountpath, do.Volume.Params["pool"], do.Volume.Name))
 	return nil
 }
 
 // Exists returns false always.
 func (d *Driver) Exists(do storage.DriverOptions) (bool, error) {
-	return false, nil
+	log.Infof("In %q", getFunctionName())
+	if _, ok := d.created[filepath.Join(d.mountpath, do.Volume.Params["pool"], do.Volume.Name)]; !ok {
+		return false, errored.Errorf("volume not created")
+	}
+	return true, nil
 }
 
 // CreateSnapshot is a noop.
 func (d *Driver) CreateSnapshot(s string, do storage.DriverOptions) error {
+	log.Infof("In %q", getFunctionName())
 	return nil
 }
 
 // RemoveSnapshot is a noop.
 func (d *Driver) RemoveSnapshot(s string, do storage.DriverOptions) error {
+	log.Infof("In %q", getFunctionName())
 	return nil
 }
 
 // CopySnapshot is a noop.
 func (d *Driver) CopySnapshot(do storage.DriverOptions, s, s2 string) error {
+	log.Infof("In %q", getFunctionName())
 	return nil
 }
 
 // ListSnapshots returns an empty list.
 func (d *Driver) ListSnapshots(do storage.DriverOptions) ([]string, error) {
+	log.Infof("In %q", getFunctionName())
 	return []string{}, nil
 }
 
 // Mounted retuns an empty list.
 func (d *Driver) Mounted(t time.Duration) ([]*storage.Mount, error) {
+	log.Infof("In %q", getFunctionName())
 	return []*storage.Mount{}, nil
 }
 
 // InternalName returns the passed string as is.
 func (d *Driver) InternalName(s string) (string, error) {
+	log.Infof("In %q", getFunctionName())
 	return s, nil
 }
 
 // InternalNameToVolpluginName returns the passed string as is.
 func (d *Driver) InternalNameToVolpluginName(s string) string {
+	log.Infof("In %q", getFunctionName())
 	return s
 }
 
 // MountPath describes the path at which the volume should be mounted.
 func (d *Driver) MountPath(do storage.DriverOptions) string {
-	return filepath.Join(d.mountpath, do.Volume.Name)
+	log.Infof("In %q", getFunctionName())
+	return filepath.Join(d.mountpath, do.Volume.Params["pool"], do.Volume.Name)
 }
