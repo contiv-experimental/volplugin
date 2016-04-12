4e9dc5e2d11c47622faf49de8e5ef6dd01de143a
Erik Hollensbe <erik+github@hollensbe.org>
2016-04-12 04:07:09 -0700

    storage/driver.go: initial interfaces for new driver structure
    Signed-off-by: Erik Hollensbe <erik+github@hollensbe.org>

--- storage/driver.go
+++ storage/driver.go
@@ -49,14 +49,40 @@ type Volume struct {
 	Params
 }
 
+// NamedDriver is a named driver and has a method called Name()
+type NamedDriver interface {
+	// Name returns the string associated with the storage backed of the driver
+	Name() string
+}
+
 // Driver is a full driver that implements a storage function available to
 // volplugin. Drivers are called anywhere filesystem operations are necessary.
 // Consumers should do this through the NewStorage call and associated member
 // functions.
 type Driver interface {
-	// Name returns the string associated with the storage backed of the driver
-	Name() string
+	NamedDriver
+	CRUDDriver
+	SnapshotDriver
+	MountDriver
+}
 
+// MountDriver mounts volumes.
+type MountDriver interface {
+	// Mount a Volume
+	Mount(DriverOptions) (*Mount, error)
+
+	// Unmount a volume
+	Unmount(DriverOptions) error
+	// Mounted shows any volumes that belong to volplugin on the host, in
+	// their native representation. They yield a *Mount.
+	Mounted(time.Duration) ([]*Mount, error)
+
+	// MountPath describes the path at which the volume should be mounted.
+	MountPath(DriverOptions) string
+}
+
+// CRUDDriver performs CRUD operations.
+type CRUDDriver interface {
 	// Create a volume.
 	Create(DriverOptions) error
 
@@ -69,15 +95,20 @@ type Driver interface {
 	// List Volumes. May be scoped by storage parameters or other data.
 	List(ListOptions) ([]Volume, error)
 
-	// Mount a Volume
-	Mount(DriverOptions) (*Mount, error)
-
-	// Unmount a volume
-	Unmount(DriverOptions) error
-
 	// Exists returns true if a volume exists. Otherwise, it returns false.
 	Exists(DriverOptions) (bool, error)
 
+	// InternalName translates a volplugin `tenant/volume` name to an internal
+	// name suitable for the driver. Yields an error if impossible.
+	InternalName(string) (string, error)
+
+	// InternalNameToVolpluginName translates an internal name to a volplugin
+	// `tenant/volume` syntax name.
+	InternalNameToVolpluginName(string) string
+}
+
+// SnapshotDriver manages snapshots.
+type SnapshotDriver interface {
 	// CreateSnapshot creates a named snapshot for the volume. Any error will be returned.
 	CreateSnapshot(string, DriverOptions) error
 
@@ -91,19 +122,4 @@ type Driver interface {
 	// CopySnapshot copies a snapshot into a new volume. Takes a DriverOptions,
 	// snap and volume name (string). Returns error on failure.
 	CopySnapshot(DriverOptions, string, string) error
-
-	// Mounted shows any volumes that belong to volplugin on the host, in
-	// their native representation. They yield a *Mount.
-	Mounted(time.Duration) ([]*Mount, error)
-
-	// InternalName translates a volplugin `tenant/volume` name to an internal
-	// name suitable for the driver. Yields an error if impossible.
-	InternalName(string) (string, error)
-
-	// InternalNameToVolpluginName translates an internal name to a volplugin
-	// `tenant/volume` syntax name.
-	InternalNameToVolpluginName(string) string
-
-	// MountPath describes the path at which the volume should be mounted.
-	MountPath(DriverOptions) string
 }
