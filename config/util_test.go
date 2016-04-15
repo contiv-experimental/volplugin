c94530db70044eb41c170a0bd9de8890be14a10b
Erik Hollensbe <erik+github@hollensbe.org>
2016-04-15 07:27:56 -0700

    config: in tests, fix etcd restarting to handle the 10s delay on start now
    Signed-off-by: Erik Hollensbe <erik+github@hollensbe.org>

--- config/util_test.go
+++ config/util_test.go
@@ -8,15 +8,9 @@ import (
 )
 
 func stopStartEtcd(c *C, f func()) {
-	defer time.Sleep(1 * time.Second)
-	defer func() {
-		// I have ABSOLUTELY no idea why CombinedOutput() is required here, but it is.
-		_, err := exec.Command("/bin/sh", "-c", "sudo systemctl restart etcd").CombinedOutput()
-		c.Assert(err, IsNil)
-	}()
-
-	c.Assert(exec.Command("/bin/sh", "-c", "sudo systemctl start etcd").Run(), IsNil)
 	c.Assert(exec.Command("/bin/sh", "-c", "sudo systemctl stop etcd").Run(), IsNil)
 	time.Sleep(1 * time.Second)
 	f()
+	c.Assert(exec.Command("/bin/sh", "-c", "sudo systemctl start etcd").Run(), IsNil)
+	time.Sleep(15 * time.Second)
 }
