15c762ceb17e7d2c9df1853268f825e5332a81f1
unclejack <unclejacksons@gmail.com>
2016-05-26 21:53:47 +0300

    volmaster: add uses endpoint to get mount state
    Signed-off-by: Cristian Staretu <cristian.staretu@gmail.com>

--- volmaster/daemon.go
+++ volmaster/daemon.go
@@ -98,14 +98,15 @@ func (d *DaemonConfig) Daemon(listen string) {
 	}
 
 	getRouter := map[string]func(http.ResponseWriter, *http.Request){
-		"/global":                      d.handleGlobal,
-		"/policies":                    d.handlePolicyList,
-		"/policies/{policy}":           d.handlePolicy,
-		"/volumes/":                    d.handleListAll,
-		"/volumes/{policy}":            d.handleList,
-		"/volumes/{policy}/{volume}":   d.handleGet,
-		"/runtime/{policy}/{volume}":   d.handleRuntime,
-		"/snapshots/{policy}/{volume}": d.handleSnapshotList,
+		"/global":                        d.handleGlobal,
+		"/policies":                      d.handlePolicyList,
+		"/policies/{policy}":             d.handlePolicy,
+		"/uses/mounts/{policy}/{volume}": d.handleUsesMountsVolume,
+		"/volumes/":                      d.handleListAll,
+		"/volumes/{policy}":              d.handleList,
+		"/volumes/{policy}/{volume}":     d.handleGet,
+		"/runtime/{policy}/{volume}":     d.handleRuntime,
+		"/snapshots/{policy}/{volume}":   d.handleSnapshotList,
 	}
 
 	for path, f := range getRouter {
@@ -221,6 +222,32 @@ func (d *DaemonConfig) handlePolicy(w http.ResponseWriter, r *http.Request) {
 	content, err := json.Marshal(policyObj)
 	if err != nil {
 		httpError(w, "Marshalling policy response", err)
+		return
+	}
+
+	w.Write(content)
+}
+
+func (d *DaemonConfig) handleUsesMountsVolume(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	policy := vars["policy"]
+	volumeName := vars["volume"]
+
+	vc := &config.Volume{
+		PolicyName: policy,
+		VolumeName: volumeName,
+	}
+
+	mount := &config.UseMount{}
+
+	if err := d.Config.GetUse(mount, vc); err != nil {
+		httpError(w, "Getting mount", err)
+		return
+	}
+
+	content, err := json.Marshal(mount)
+	if err != nil {
+		httpError(w, "Marshalling response", err)
 		return
 	}
 
