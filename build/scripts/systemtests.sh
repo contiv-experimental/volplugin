786513804eea9107e759635da898b18f3a549698
Madhav Puri <madhav.puri@gmail.com>
2016-07-19 15:11:45 -0700

    build/scripts: fix systemtest logic
    - remove the loop otherwise the tests run twice
    - also remove setting GOPATH for CI environment, it is
      better to control it from higher level jenkins job or make target
    
    Signed-off-by: Madhav Puri <madhav.puri@gmail.com>

--- build/scripts/systemtests.sh
+++ build/scripts/systemtests.sh
@@ -7,13 +7,5 @@ then
   export USE_DRIVER=ceph
 fi
 
-#set GOPATH in CI environment
-if [ "x${WORKSPACE}" != "x" ]; then
-    export GOPATH=${WORKSPACE}
-fi
-
-for i in ceph nfs
-do
-  echo running ${USE_DRIVER}-driver tests...
-  go test -v -timeout 240m ./systemtests -check.v -check.f "${TESTRUN}"
-done
+echo running ${USE_DRIVER}-driver tests...
+go test -v -timeout 240m ./systemtests -check.v -check.f "${TESTRUN}"
