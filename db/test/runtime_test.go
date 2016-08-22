13530edb697ef0df8ba731ac255ccd9e4ecee7f8
Erik Hollensbe <github@hollensbe.org>
2016-08-22 09:49:37 -0700

    db/test/runtime_test.go: new test for runtime watches
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

---
+++ db/test/runtime_test.go
@@ -0,0 +1,27 @@
+package test
+
+import (
+	"github.com/contiv/volplugin/db"
+	. "gopkg.in/check.v1"
+)
+
+func (s *testSuite) TestRuntimeWatch(c *C) {
+	c.Assert(s.client.Set(testPolicies["basic"]), IsNil)
+	policy := db.NewPolicy("basic")
+	c.Assert(s.client.Get(policy), IsNil)
+
+	vol, err := db.CreateVolume(&db.VolumeRequest{Policy: policy, Name: "bar"})
+	c.Assert(err, IsNil)
+
+	objChan, errChan := s.client.WatchAll(&db.RuntimeOptions{})
+	opts := db.NewRuntimeOptions(vol.PolicyName, vol.VolumeName)
+	opts.RateLimit.ReadBPS = 1000
+	c.Assert(s.client.Set(opts), IsNil)
+
+	select {
+	case err := <-errChan:
+		c.Assert(err, IsNil)
+	case obj := <-objChan:
+		c.Assert(obj.(*db.RuntimeOptions).RateLimit.ReadBPS, Equals, uint64(1000))
+	}
+}
