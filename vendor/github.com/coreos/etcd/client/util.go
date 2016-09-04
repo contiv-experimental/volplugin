2b3bc3130acda200a4ac2fea9d0316abbd0e371b
Erik Hollensbe <github@hollensbe.org>
2016-09-04 13:19:46 -0700

    Godeps,vendor: upgrade to etcd v3.0.7 client
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- vendor/github.com/coreos/etcd/client/util.go
+++ vendor/github.com/coreos/etcd/client/util.go
@@ -1,4 +1,4 @@
-// Copyright 2015 The etcd Authors
+// Copyright 2016 The etcd Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,11 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package types
+package client
 
-// Uint64Slice implements sort interface
-type Uint64Slice []uint64
-
-func (p Uint64Slice) Len() int           { return len(p) }
-func (p Uint64Slice) Less(i, j int) bool { return p[i] < p[j] }
-func (p Uint64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+// IsKeyNotFound returns true if the error code is ErrorCodeKeyNotFound.
+func IsKeyNotFound(err error) bool {
+	if cErr, ok := err.(Error); ok {
+		return cErr.Code == ErrorCodeKeyNotFound
+	}
+	return false
+}
--- vendor/github.com/coreos/etcd/pkg/types/doc.go
+++ vendor/github.com/coreos/etcd/pkg/types/doc.go
@@ -12,11 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package types declares various data types and implements type-checking
+// functions.
 package types
-
-// Uint64Slice implements sort interface
-type Uint64Slice []uint64
-
-func (p Uint64Slice) Len() int           { return len(p) }
-func (p Uint64Slice) Less(i, j int) bool { return p[i] < p[j] }
-func (p Uint64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
